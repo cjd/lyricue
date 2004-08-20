cd1f7888b02ccbe2fb6d75ede25341d9e3927bcf
cjdebenh <>
2004-08-20 00:50:54 +0000

    Initial import for moving media to the db rather than filesystem

--- mysql/Update_1.9.2.sql
+++ mysql/Update_1.9.2.sql
@@ -1,3 +1,13 @@
-use lyricDb;
+CREATE DATABASE mediaDb;
 
-ALTER TABLE playlist ADD transition int(11) default '0';
+use mediaDb;
+
+CREATE TABLE media (
+  id int(10) unsigned NOT NULL auto_increment,
+  category varchar(100) NOT NULL default '',
+  subcategory varchar(100) NOT NULL default '',
+  type varchar(10) NOT NULL default '',
+  description varchar(100) NOT NULL default '',
+  data blob,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
