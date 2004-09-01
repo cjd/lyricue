d7a703a2529c3647822576213df619d84d3b2683
cjdebenh <>
2004-09-01 05:29:28 +0000

    More work on import media

--- mysql/Update_1.9.2.sql
+++ mysql/Update_1.9.2.sql
@@ -7,7 +7,12 @@ CREATE TABLE media (
   category varchar(100) NOT NULL default '',
   subcategory varchar(100) NOT NULL default '',
   type varchar(10) NOT NULL default '',
-  description varchar(100) NOT NULL default '',
-  data blob,
-  PRIMARY KEY  (id)
+  format varchar(10) NOT NULL default '',
+  insertedby varchar(100) NOT NULL default '',
+  insertdate datetime NOT NULL default '0000-00-00 00:00:00',
+  description varchar(100) default '',
+  data mediumblob,
+  PRIMARY KEY  (id),
+  UNIQUE KEY data (data(100),description,category,subcategory,type,format)
 ) TYPE=MyISAM;
+
